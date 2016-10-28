1f506075d6c14ec6a28b4cba8dc69d33d77d969c
seeluck <810201584@qq.com>
2016-10-28 16:42:40 +0800

    123

--- 1. Two Sum/1. Two Sum.js
+++ 1. Two Sum/1. Two Sum.js
@@ -21,7 +21,7 @@ var twoSum1 = function(nums, target) {
 /**高手的
  * tmp用来记录目标与数组中每一个数的差值
  * 把每个数当做一个新数组a的key,value为原数组的key
- * 每一次循环都会打一个新的数字进入新的数组a,所以从第二次循环开始才是真正的比对
+ * 每一次循环都会push一个新的数字进入新的数组a,所以从第二次循环开始才是真正的比对
  * 当tmp作为key存在于新数组a里时,返回此时的value即在原数组的key和正在比对的key即结果
  */
 var twoSum2 = function(nums, target) {
